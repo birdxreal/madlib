308fe0e829e67b021ae3b727e03456c93505eda7
Bird Xreal <saraichuchi@gmail.com>
2020-06-20 21:31:58 -0400

    second commit

--- mad.py
+++ mad.py
@@ -1,5 +1,10 @@
 
 color = input("Enter a color: ")
-plural_noun = input()
+plural_noun = input("Enter a plural Noun: ")
+celebrity = input("Enter a celebrity: ")
+
+print("Roses are" + color)
+print(plural_noun + " are blue")
+print("I love" + celebrity)
 
 
